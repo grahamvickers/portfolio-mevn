7b6a292b760de35d3af8c67aedee6099855b5aed
Graham Vickers <43250387+grahamvickers@users.noreply.github.com>
2022-03-21 21:55:48 -0400

    project view is styled

--- front-end/src/views/project.vue
+++ front-end/src/views/project.vue
@@ -1,7 +1,30 @@
 <template>
   <div>
-    
-    <router-link to="/">Back to home</router-link>
+    <div class="secHeading">
+          <h3>Featured Project</h3>
+          <hr>
+    </div>
+    <section>
+      <div>
+        <div>
+          <img :src="`${project.logo}`" alt="Project Logo" id="logo">
+          <div>
+            <h2>{{project.name}}</h2>
+            <h3 class="thinner">{{project.bio}}</h3>
+            <h5>{{project.tools}}</h5>
+          </div>
+        </div>
+        <div>
+          <p>{{project.description}}</p>
+          <a target="_blank" :href="`${project.github}`" class="button"><span><img src="static/github_mint.svg" alt="Github Project Link icon">View on Github</span></a>
+        </div>
+      </div>
+      <img :src="`${project.mockup}`" alt="Project IPhone XR Mockup">
+      
+      
+        
+    </section>
+
   </div>
 </template>
 
@@ -27,3 +50,24 @@ export default {
 };
 
 </script>
+
+<style lang="scss" scoped>
+#logo{
+  max-width: 260px;
+  margin-bottom: 30px;
+}
+h2,h3{
+  padding-bottom: 10px;
+}
+section{
+  margin-bottom: 80px;
+}
+
+@media screen and (min-width: 1200px) {
+  section{
+    display: flex;
+    flex-direction: row;
+    padding-bottom: 160px;
+  }
+}
+</style>
